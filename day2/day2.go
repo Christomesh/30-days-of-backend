2a377ecdbfc1a6c0e3e4d3604757d2c4befaf90b
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-12-20 12:29:49 +0100

    added day 3 challenge

---
+++ day2/day2.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func LeapYear(Y int, N int) {
+	var lpy []int
+	count := 0
+	year := Y
+
+	for count < N {
+		if (year%4 == 0 && year%100 != 0) || (year%400 == 0) {
+			lpy = append(lpy, year)
+			count++
+		}
+		year++
+	}
+	fmt.Println(lpy)
+}
+
+func main() {
+	LeapYear(2020, 4)
+}
